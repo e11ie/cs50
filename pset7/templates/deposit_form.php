7974c6bfd6b93c60c3c5d04fb9549ede6808865a
Jessie <jessieadrian3@gmail.com>
2015-02-01 23:51:18 -0600

    add deposit_form.php

---
+++ pset7/templates/deposit_form.php
@@ -0,0 +1,12 @@
+<form action="deposit.php" method="post">
+    <fieldset>
+        <div class="form-group inline">
+            I want to deposit this amount of cash into my account: 
+            <br><br>
+            $<input autofocus class="form-control" name="dollars" placeholder="0" type="text"/> .00
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Deposit</button>
+        </div>
+    </fieldset>
+</form>
