32571c73407f17ced09e021b9507fd42bd13a88d
Jessie <jessieadrian3@gmail.com>
2015-02-01 23:50:48 -0600

    add buy_form.php

---
+++ pset7/templates/buy_form.php
@@ -0,0 +1,11 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="form-group">I want to buy 
+            <input autofocus class="form-control" name="shares" placeholder="Shares" type="text"/> from
+            <input autofocus class="form-control" name="symbol" placeholder="Symbol" type="text"/> 
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Buy</button>
+        </div>
+    </fieldset>
+</form>
