4872b2e8a4c32af92f22e539cc8bd4786dd3b5dc
Jessie <jessieadrian3@gmail.com>
2015-02-01 23:55:16 -0600

    add settings_form.php

---
+++ pset7/templates/settings_form.php
@@ -0,0 +1,15 @@
+<div>Change Password here:
+</div>
+<form action="settings.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="password" placeholder="New Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="confirmation" placeholder="Confirmation" type="password"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Change Password</button>
+        </div>
+        </fieldset>
+</form>
