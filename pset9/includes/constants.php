ecf85fc97be7561109229f7ef1bb0e9ff95029d2
Jessie <jessieadrian3@gmail.com>
2015-02-01 23:59:02 -0600

    add constants.php

---
+++ pset9/includes/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * CS50
+     * Problem Set 9: Final Project
+     *
+     * Global constants.
+     */
+
+    // password protect db? (for final don't need to bc I don't need to sumbit the code!!!!)
+    
+    // Configuration
+    $dbhost = 'localhost';
+    $dbname = 'petdb';
+    
+    // to use these (and other variables defined and used outside of functions) in functions
+    // do this: $variable_name = GLOBALS["dbname"];
+
+?>
